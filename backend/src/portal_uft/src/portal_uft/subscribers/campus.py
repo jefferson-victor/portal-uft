988354b5623d9064b061c50f72493b691898b2d3
Jefferson Victor <jefferson.victor@gmail.com>
2022-06-30 20:44:18 -0300

    Exercicio 24

--- backend/src/portal_uft/src/portal_uft/subscribers/campus.py
+++ backend/src/portal_uft/src/portal_uft/subscribers/campus.py
@@ -1,12 +1,13 @@
 from kitconcept import api
 from portal_uft.content.campus import Campus
 from zope.lifecycleevent import ObjectAddedEvent
+from zope.lifecycleevent import ObjectModifiedEvent
 
 
 def _update_tags(obj: Campus):
     """Update tags on Campus object."""
     vocab = api.vocabulary.get("portal_uft.vocabulary.cities")
-    tags = set(obj.subject)
+    tags = {tag for tag in obj.subject if not tag.startswith("Campus: ")}
     city = obj.city
     term = vocab.getTermByToken(city)
     tags.add(f"Campus: {term.title}")
@@ -15,5 +16,11 @@ def _update_tags(obj: Campus):
 
 def added(obj: Campus, event: ObjectAddedEvent):
     """A new Campus was added to the site."""
+    # Atualiza tags do objeto
+    _update_tags(obj)
+
+
+def modified(obj: Campus, event: ObjectModifiedEvent):
+    """A Campus object was modified."""
     # Atualiza tags do objeto
     _update_tags(obj)
